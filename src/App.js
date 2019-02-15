08430bc642c8b31186d6925fc2cc2d5775744624
shyamraja <t7bhsh00@students.oamk.fi>
2019-02-15 18:22:25 +0200

    changes made  for uploading thesis

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './styles/upload-thesis.css';
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { BrowserRouter as Router, Route } from "react-router-dom";
--- src/components/upload-thesis/UploadThesis.js
+++ src/components/upload-thesis/UploadThesis.js
@@ -6,20 +6,72 @@ import { connect } from "react-redux";
 class UploadThesis extends Component {
   render() {
     return (
-      <div>
-        <p>Upload thesis component </p>
-      </div>
+      <div className="upload-thesis">
+        <div className="container">
+
+
+        <h2>UPLOAD YOUR THESIS</h2>
+
+        <form>
+          <table id="UploadTable"
+              class="table table-bordered
+                     table-condensed table-striped">
+        <thead>
+          <tr>
+            <th><button type="button" class="btn btn-success btn-lg btn-block">SELECT  YOUR  THESIS</button></th>
+            <th><input type="file" name="datafile"></input></th>
+           <th><button type="button" class="btn btn-danger">REMOVE</button></th>
+          </tr>
+          <br>
+          <tr>
+        <th><button type="button" class="btn btn-success btn-lg btn-block">SUPPLEMENTARY DOCUMENTS</button></th>
+        <th><input type="file" name="datafile"></input></th>
+        <th><button type="button" class="btn btn-danger">REMOVE</button></th>
+       </tr>
+            <br>
+            <tr>
+            <th><button type="button" class="btn btn-success btn-lg btn-block">FIELD  VISIT  PHOTOGRAPHS</button></th>
+           <th><input type="file" name="datafile"></input> size="40"></th>
+          <th><button type="button" class="btn btn-danger">REMOVE</button></th>
+
+         </tr>
+            </br>
+            </br>
+            
+
+        </thead>
+      </table>
+
+    
+
+  <div class="checkbox">
+      <input type="checkbox" name="thesis" value=""></input>I conform the given information is correct<br>
+      <input type="checkbox" name="thesis" value=""></input>I agree the terms and conditions<br>
+  </br>
+  </br>
+   </div>
+</form>
+  <div class="button">
+      <a href="#" class="previous"> BACK</a>
+      <a href="#" class="next">SAVE AND CONTINUE LATER &raquo;</a>
+      <a href="#" class="next">NEXT &raquo;</a>
+  </div>
+
+</div>
+</div>
+       
     );
   }
 }
-
+export default UploadThesis;
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = {};
 
 UploadThesis.propTypes = {};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UploadThesis);
+//export default connect(
+ //mapStateToProps,
+//mapDispatchToProps
+//  )
+//(UploadThesis);
